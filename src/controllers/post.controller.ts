933a6dd5b40852660aab32aee90ecc48ed0c58df
BlueBird <kirk.maningat@gmail.com>
2022-09-12 20:50:26 +0900

    testing postman

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -2,8 +2,9 @@ import {Request, Response} from 'express'
 
 import {connect} from '../database'
 
-export async function getPosts(req: Request, res: Response) {
+export async function getPosts(req: Request, res: Response): Promise<Response> {
         const conn = await connect();
-        conn.query('SELECT * FROM posts');
+        const posts = await conn.query('SELECT * FROM posts');
+        return res.json(posts[0]);
 
 }
--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,7 +1,9 @@
 import {Router} from 'express'
 const router = Router();
 
+import { getPosts } from '../controllers/post.controller'
+
 router.route('/')
-    .get()
+    .get(getPosts);
 
 export default router;
